29f782ccde8933d9992a7e1cb34c14be18724dc0
mrabtini <mrabtiniaymen@outlook.com>
2019-01-30 13:13:59 +0100

    projet

---
+++ compte_bancaire/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compte_bancaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            compte c = new compte(25,7,"dhjy","M",700,200,100);
+           
+            
+                Console.WriteLine("1-decouvert");
+                Console.WriteLine("2-debuter");
+                Console.WriteLine("3-crediter");
+                Console.WriteLine("donner votre choix");
+                int i =int.Parse(Console.ReadLine());
+                switch(i){
+                    case 1:c.decouvert();
+                        break;
+                        case 2:c.debuter(2.5);
+                        break;
+                        case 3:c.crediter(6.5);break;
+                        default:Console.WriteLine("erreur");break;
+                         }
+
+
+
+
+
+     
+
+        }
+    }
+}
---
+++ compte_bancaire/compte.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compte_bancaire
+{
+    class compte
+    {private int num;
+     private static int nbcompte;
+     private string prt;
+     private string typeprt;
+     private double solde;
+     private double decmax;
+     private double debmax;
+
+     public int Num
+     {
+         get
+         {
+             return num;
+         }
+     }
+
+     public static int Nbcompte
+     {
+         get { return nbcompte; }
+     }
+
+
+     public string Prt
+     {
+         get { return prt; }
+     }
+
+
+
+     public string Typeprt
+     {
+         get { return typeprt; }
+         set { typeprt = value; }
+
+     }
+
+        public double Solde
+        {
+            get { return solde; }
+            set { solde = value; }
+        }
+
+        public double Decouvmax
+        {
+            get { return decmax; }
+            set { decmax = value; }
+
+        }
+
+        public double Debmax
+        {
+            get { return debmax; }
+            set { debmax = value; }
+        }
+
+
+        public compte(int a , int b, string c,string d , double e, double f,double j)
+        {
+            num = a;
+            nbcompte = b;
+            prt = c;
+            typeprt = d;
+            solde = e;
+            do
+            {
+                decmax = f;
+            } while (f <= solde);
+            debmax = j;
+        }
+        
+        public double decouvert()
+        {
+            if (this.solde >= 0)
+            {
+             
+                return (0);
+            }
+            
+            
+                return(-solde);
+            
+        }
+
+
+        public double crediter(double s)
+        {
+            do { solde = solde + s; }
+            while (s > 0);
+            return (solde);
+        }
+        public double debuter(double s)
+        {
+            do
+            {
+                solde = solde - s;
+            } while (solde >= decmax);
+            return (solde);
+        }
+
+    }
+}
